888aaf5602aee888597c257e33966a107794911a
John Harper <john@harper.link>
2025-01-29 13:35:22 -0600

    Implement year 2019, day01, parts 1 and 2

---
+++ aoc2019/day01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+var lines = File.ReadAllLines("input.txt");
+var masses = lines.Select(int.Parse).ToList();
+
+var fuelAmounts = masses.Select(mass => CalcFuel(mass));
+var part1 = fuelAmounts.Sum();
+var fuelAmountsPart2 = masses.Select(mass => CalcFuelPart2(mass));
+var part2 = fuelAmountsPart2.Sum();
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+
+
+int CalcFuel(int mass)
+{
+    return Math.Max(mass / 3 - 2, 0);
+}
+
+int CalcFuelPart2(int mass)
+{
+    var fuel = CalcFuel(mass);
+
+    int additionalFuel = CalcFuel(fuel);
+    while (additionalFuel > 0)
+    {
+        fuel += additionalFuel;
+        additionalFuel = CalcFuel(additionalFuel);
+    }
+
+    return fuel + additionalFuel;
+}

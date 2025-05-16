d71b21b0fd684e1c7824bb15a664587cd0337536
John Harper <john@harper.link>
2025-05-15 22:05:36 -0500

    day 12 2024 incomplete

---
+++ aoc2024/day12/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day12;
+
+class Program
+{
+  static string[] lines = new string[0];
+  static int WIDTH = -1;
+  static int HEIGHT = -1;
+
+  static void Main(string[] args)
+  {
+    lines = File.ReadAllLines("input-sample.txt");
+    WIDTH = lines[0].Length;
+    HEIGHT = lines.Length;
+
+  }
+
+  static char get((int, int) pos)
+  {
+    if (pos.Item1 < 0 || pos.Item1 >= WIDTH || pos.Item2 < 0 || pos.Item2 >= HEIGHT)
+    {
+      return ' ';
+    }
+    return lines[pos.Item2][pos.Item1];
+  } 
+}
